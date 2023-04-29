f4a1862817165af867d053469211db455e299b5a
trehanpraharsh <trehanpraharsh@gmail.com>
2023-04-30 01:24:51 +0530

    Understanding package json file

---
+++ 12-understandingPackage.js
@@ -0,0 +1,16 @@
+// npm --global command, comes with node
+//* npm --version
+
+// local dependency - use it only in this particular project
+//* npm i <packageName>
+
+// global dependency - use it any project
+//* npm install -g <packageName>
+//* sudo npm install -g <packageName> (mac)
+
+//* package.json - manifest file (stores important info about the package / project)
+// manual approach (create package.json in the root, create properties etc)
+//* npm init (step by step, press enter to skip)
+//* npm init -y (everything by default)
+
+//* npm install nodemon -D (or --save-dev, this installed as dev dependency)
